f6ab7512023eb557d2902464311d29bedc042a7e
Igor Kislitsyn <k.yngvarr@gmail.com>
2022-02-19 15:57:32 +0700

    so lazy, so effective

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 function init() {
+    const num = document.getElementById('number');
+    const more = document.getElementById('more');
+    const less = document.getElementById('less');
+
+    let coins = 8;
+    num.innerText = coins;
+
+    more.addEventListener('click', () => num.innerText = `${++coins}`);
+    less.addEventListener('click', () => num.innerText = `${coins == 0 ? coins : --coins}`);
 }
